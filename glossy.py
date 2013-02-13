78385e2e9c17095a507425887626e56a27c78417
Erik Svedäng <erik.svedang@gmail.com>
2013-02-13 14:03:00 +0100

    cleanup

--- glossy.py
+++ glossy.py
@@ -6,6 +6,8 @@ import sys
 
 CONSOLE_COLOR_ERROR = "\033[91m"
 CONSOLE_COLOR_NORMAL = "\033[0m"
+CONSOLE_COLOR_LANG_A = '\033[94m'
+CONSOLE_COLOR_LANG_B = '\033[92m'
 
 LANG_A = "isl"
 LANG_B = "sve"
@@ -48,45 +50,53 @@ def print_result():
 	print "Result:"
 	[print_item(item) for item in sorted(words, key = lambda(item): item["score"])]
 
-words = []
+def read_words_from_file(filename):
+	words = []
+	word_list_file = open(filename, "r")
+	lines = word_list_file.readlines()
+	word_list_file.close()
 
-word_list_file = open("word_list.txt", "r")
-lines = word_list_file.readlines()
-word_list_file.close()
+	i = 0
+	while i < len(lines):
+		try:
+			word_a = lines[i].strip()
+			word_b = lines[i + 1].strip()
+			empty = lines[i + 2].strip()
+		except:
+			pass
 
-i = 0
-while i < len(lines):
-	try:
-		word_a = lines[i].strip()
-		word_b = lines[i + 1].strip()
-		empty = lines[i + 2].strip()
-	except:
-		pass
+		if word_a == "":
+			print "Error reading file on line " + str(i)
+		elif word_b == "":
+			print "Error reading file on line " + str(i + 1)
+		elif empty != "":
+			print "Error reading file on line " + str(i + 2)
 
-	if word_a == "":
-		print "Error reading file on line " + str(i)
-	elif word_b == "":
-		print "Error reading file on line " + str(i + 1)
-	elif empty != "":
-		print "Error reading file on line " + str(i + 2)
+		item = {
+			LANG_A: word_a,
+			LANG_B: word_b,
+			"score": 0
+		}
 
-	item = {
-		LANG_A: word_a,
-		LANG_B: word_b,
-		"score": 0
-	}
-	words.append(item)
-	i += 3
+		words.append(item)
+		i += 3
 
-practice_count = 3 * len(words)
+	return words
 
-while(practice_count > 0):
-	item = pick_word()
-	language = pick_language()
-	word = item[language]
-	sys.stdout.write(word + " = ")
-	guess = raw_input("")
-	analyze(guess, item, language)
-	practice_count -= 1
+def practice_loop(count):
+	while(count > 0):
+		item = pick_word()
+		language = pick_language()
+		word = item[language]
+		sys.stdout.write(CONSOLE_COLOR_LANG_A + "[" + language + "] " + CONSOLE_COLOR_NORMAL + word + " = ")
+		guess = raw_input("")
+		analyze(guess, item, language)
+		count -= 1
 
+
+
+# Go!
+words = read_words_from_file("word_list.txt")
+practice_count = 1 * len(words)
+practice_loop(practice_count)
 print_result()
